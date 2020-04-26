266be2f5c32fe635126d989535ea37adb5e549e5
Mikhail <rodionovmv@gmail.com>
2020-04-26 19:42:11 +0200

    Update Django_AWS_EB_2.ps1

--- Django_AWS_EB_2.ps1
+++ Django_AWS_EB_2.ps1
@@ -1,4 +1,33 @@
- #Configure Django project for EB
- pip freeze > requirements.txt / clean up
- mkdir .ebextensions
- nano .ebextensions/django.config
+#Configure Django project for EB
+#Create requirements.txt file
+pip freeze > requirements.txt
+#Remove from requirements.txt everything except Django==2.1.1
+nano requirements.txt
+#Create hidden directory .ebextensions
+mkdir .ebextensions
+#Create django.config file in .ebextension directory, with the following content:
+#option_settings:
+#  aws:elasticbeanstalk:container:python:
+#    WSGIPath: ebdjango/wsgi.py
+nano .ebextensions/django.config
+#Deactivate virtual environment 
+deactivate
+#Install awsebcli
+sudo pip3 install awsebcli
+#Initialize EB environment
+eb init -p python-3.6 django-test-app 
+#Create EB app
+eb create django-test-app
+#Show EB app status to get CNAME
+eb status
+#Edit settings.py to add CNAME into ALLOWED_HOSTS and STATIC_ROOT = 'static' 
+nano ebdjango/settings.py
+#Activate virtual environment
+. ~/eb-virt/bin/activate
+#Collect static and create admin user
+python manage.py collectstatic
+python manage.py createsuperuser
+#Deploy Django app to EB
+eb deploy
+#Open Deployed app
+eb open
