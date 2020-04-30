cbb7a9c90c62311b4f185225568cac20c46d5186
Mikhail <rodionovmv@gmail.com>
2020-04-30 11:28:23 +0200

    Update Django_AWS_EB_3_S3_Bucket_Variables.ps1

--- Django_AWS_EB_3_S3_Bucket_Variables.ps1
+++ Django_AWS_EB_3_S3_Bucket_Variables.ps1
@@ -4,3 +4,15 @@ AWS_STORAGE_BUCKET_NAME = 'BUCKET_NAME'
 AWS_S3_REGION_NAME = 'REGION_NAME'  # e.g. us-east-2
 AWS_ACCESS_KEY_ID = 'xxxxxxxxxxxxxxxxxxxx'
 AWS_SECRET_ACCESS_KEY = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
+#Indicates django-storages the domain to use to refer to static files.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_DEFAULT_ACL = None
+
+#Where to store files (bucket folder name):
+STATICFILES_LOCATION = 'static'
+#Storage type
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
+MEDIAFILES_LOCATION = 'media'
+#Storage type
+MEDIAFILES_STORAGE = 'custom_storages.MediaStorage'
