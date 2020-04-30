a2b31813a0c98f1d8d777db0fa665a19804d84e5
Mikhail <rodionovmv@gmail.com>
2020-04-30 11:04:59 +0200

    Create Django_AWS_EB_3_S3_Bucket_Variables.ps1

---
+++ Django_AWS_EB_3_S3_Bucket_Variables.ps1
@@ -0,0 +1,6 @@
+#Variables to be placed in Django project settings.py file
+#To define bucket connection details
+AWS_STORAGE_BUCKET_NAME = 'BUCKET_NAME'
+AWS_S3_REGION_NAME = 'REGION_NAME'  # e.g. us-east-2
+AWS_ACCESS_KEY_ID = 'xxxxxxxxxxxxxxxxxxxx'
+AWS_SECRET_ACCESS_KEY = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
